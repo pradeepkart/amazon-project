3d66bfbfa1a6769ed17aa5856367dd224009db05
Pradeep_PK <pradeekarthikeyan2004@gmail.com>
2025-08-06 02:32:54 +0530

    load products from backend

--- data/products.js
+++ data/products.js
@@ -89,6 +89,32 @@ const object3 = {
 object3.method();
 */
 
+
+export let products = [];
+
+export function loadProducts(fun){
+  const xhr = new XMLHttpRequest();
+xhr.addEventListener('load',()=>{
+  products = JSON.parse(xhr.response).map((productDetails) => {
+    if (productDetails.type === 'clothing') {
+      return new Clothing(productDetails);
+    }
+    return new Product(productDetails);
+  });
+  
+  console.log('load products');
+fun();
+});
+
+
+  xhr.open('Get','https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -756,3 +782,4 @@ export const products = [
 });
 
 console.log(products);
+*/
