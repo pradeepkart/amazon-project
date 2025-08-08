4dd137d5eac4f9cec2bc72a54d01e88f2106c0b4
Pradeep_PK <pradeekarthikeyan2004@gmail.com>
2025-08-09 04:07:51 +0530

    convert loadproducts from xhr to fetch

--- jasmine/checkout/orderSummaryTest.js
+++ jasmine/checkout/orderSummaryTest.js
@@ -1,6 +1,6 @@
 import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
 import {loadFromStorage, cart} from '../../data/cart.js';
-import { loadProducts } from '../../data/products.js';
+import { loadProducts,loadProductsFetch } from '../../data/products.js';
 
 
 describe('test suite: renderOrderSummary', () => {
@@ -8,8 +8,9 @@ describe('test suite: renderOrderSummary', () => {
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
 beforeAll((done)=>{
-  loadProducts(()=>{
-    done();                  /*done() is used for flow go to the next step after load the backend*/
+  loadProductsFetch().then(()=>{
+done();
+                /*done() is used for flow go to the next step after load the backend*/
   });
  
 });
